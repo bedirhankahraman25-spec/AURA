a1823a542dfe142e8c36c0127bf3a680dd00c1e3
bedirhankahraman25-spec <bedirhankahraman25@gmail.com>
2026-08-13 17:08:08 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,19 @@
-const CACHE_NAME = "aura-v1";
-
-const FILES = [
-    "./",
-    "./index.html",
-    "./manifest.json"
-];
-
 self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES);
-        })
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => caches.delete(key))
+            )
+        )
     );
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+    event.respondWith(fetch(event.request));
 });
