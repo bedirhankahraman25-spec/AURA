371386ca6efd7740d3290afebf7387117ba4ba1f
bedirhankahraman25-spec <bedirhankahraman25@gmail.com>
2026-08-13 16:41:23 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,12 +12,28 @@ self.addEventListener("install", event => {
             return cache.addAll(FILES);
         })
     );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
         })
     );
 });
